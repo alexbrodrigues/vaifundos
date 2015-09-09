309e76c554d7f97253730e59909e646d074cb0cb
alexbrodrigues <6666alex@gmail.com>
2015-09-09 18:06:47 -0300

    Correção da Classe Fundo de Investimento Dolar e Incrementação da Main

---
+++ Vaifundos/Vaifundos/Fundo_Investimento_Dolar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaifundos
+{
+    class Fundo_Investimento_Dolar: Fundo_de_Investimento
+    {
+        private float Dolar;
+
+        public float Dolar1
+        {
+            get { return Dolar; }
+            set { Dolar = value; }
+        }
+    }
+}
--- Vaifundos/Vaifundos/Program.cs
+++ Vaifundos/Vaifundos/Program.cs
@@ -11,15 +11,27 @@ namespace Vaifundos
         static void Main(string[] args)
         {
             List<Clientes> List_cliente;
-            Clientes teste = null;
+            Clientes clientecadastro = null;
+            int cont = 0;
+            int opcaoinicial = 0;
             int opcao = 0;
             Console.WriteLine("*******      Bem Vindo ao Sistema Vai Fundos     *******");
-            Console.WriteLine("Se deseja efetuar cadastro de cliente digite 1.");
-            Console.WriteLine("Se deseja movimentar fundo de Investimento digite 2");
-            opcao = Convert.ToInt32(Console.ReadLine());
-            while (opcao == 1)
+            Console.WriteLine("Se deseja efetuar operações de Banco digite 1, se deseja efetuar operações de Cliente digite 2:");
+            if (opcaoinicial == 1)
             {
-
+                Console.WriteLine("Se deseja efetuar cadastro de cliente digite 1.");
+                Console.WriteLine("Se deseja efetuar cadastro de fundo digite 2.");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                while (opcao == 1)
+                {
+                    Console.WriteLine("Entre com o nome do Cliente:");
+                    clientecadastro.Nome = Console.ReadLine();
+                    clientecadastro.Id = cont;
+                    cont++;
+                    Console.WriteLine("Deseja efetuar novo Cadastro? Se sim insira 1 novamente");
+                    Console.WriteLine("Se deseja efetuar cadastro de fundo digite 2.");
+                    opcao = Convert.ToInt32(Console.ReadLine());
+                }
             }
         }
     }
