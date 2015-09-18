2cb1278594b923d29d2b3caf2fb3580b363952ab
alexbrodrigues <6666alex@gmail.com>
2015-09-18 19:22:24 -0300

    Criação da Classe Animal

---
+++ Viveiro/Viveiro/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viveiro
+{
+    abstract class Animal
+    {
+        private string nome;
+        private DateTime dataNascimento;
+        private float peso;
+        private float capEstomago;
+
+    }
+}
