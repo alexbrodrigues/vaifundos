5c2712953898ea9101d6f0adcdf15258f781a7cd
alexbrodrigues <6666alex@gmail.com>
2015-09-18 21:28:35 -0300

    Criação de Classes Gato, Gavião, Curió,Cachorro  e Mamifero

---
+++ Viveiro/Viveiro/Mamifero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viveiro
+{
+    class Mamifero: Animal
+    {
+        private int qtdGlandulasMamarias;
+
+        public int QtdGlandulasMamarias
+        {
+            get
+            {
+                return qtdGlandulasMamarias;
+            }
+
+            set
+            {
+                qtdGlandulasMamarias = value;
+            }
+        }
+
+        public override bool Comer(float qtd)
+        {
+            while (qtd < CapEstomago)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
