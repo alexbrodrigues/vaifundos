6b62386117a947afc7c8eea99f4988685b8f2d19
alexbrodrigues <6666alex@gmail.com>
2015-10-14 18:51:52 -0300

    Criação da Interface Ordenar

---
+++ Batalha_Naval/Batalha_Naval/IPodeOrdenar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batalha_Naval
+{
+    interface IPodeOrdenar
+    {
+        public void Ordenar (Object ordenador)
+    }
+}
