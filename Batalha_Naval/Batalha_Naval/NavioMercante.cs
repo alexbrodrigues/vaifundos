311b88cfd9fc6504df560d9968e85946a8081672
alexbrodrigues <6666alex@gmail.com>
2015-10-14 17:13:32 -0300

    Criação da Classe Navio Mercante

---
+++ Batalha_Naval/Batalha_Naval/NavioMercante.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batalha_Naval
+{
+    class NavioMercante
+    {
+        private float carga;
+
+        public float Carga
+        {
+            get { return carga; }
+            set { carga = value; }
+        }
+        private float capacidadeCarga;
+
+        public float CapacidadeCarga
+        {
+            get { return capacidadeCarga; }
+            set { capacidadeCarga = value; }
+        }
+
+        public void Carregamento()
+        {
+            float volumecarga = 0;
+            volumecarga = carga / capacidadeCarga;
+            Console.WriteLine("O Navio Mercante possui {0} de carga, {1} capacidade de carga e {2} de volume ocupado",carga,capacidadeCarga,volumecarga);
+        }
+    }
+}
